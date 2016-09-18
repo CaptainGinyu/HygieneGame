b3b5aea95b526975242c60e01d603dcccea2ec1e
CaptainGinyu <kvn_chow@berkeley.edu>
2016-09-17 22:41:16 -0700

    Smoky fire :fire:

---
+++ Assets/Assets/Scripts/OnOffSource.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnOffSource : MonoBehaviour
+{
+	public string nameOfThis;
+	public float delayLowerBound;
+	public float delayUpperBound;	
+	public GameObject attachment;
+
+	private float nextSwitchingTime;
+
+	void Start()
+	{
+		nextSwitchingTime = 0.0f;
+	}
+
+	void Update()
+	{
+		if (Time.time > nextSwitchingTime)
+		{
+			attachment.SetActive(!attachment.activeSelf);
+			if (delayLowerBound < delayUpperBound)
+			{
+				nextSwitchingTime = Time.time + Random.Range(delayLowerBound, delayUpperBound);
+			}
+			else
+			{
+				nextSwitchingTime = Time.time + delayLowerBound;
+			}
+		}
+	}
+}
